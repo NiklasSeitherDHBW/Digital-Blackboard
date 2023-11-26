c1a0e1b38c7e08ca5d5e5c4e145b260b18b9e89c
DE130063v002 <s222792@student.dhbw-mannheim.de>
2023-11-26 18:59:17 +0100

    Added missing files

---
+++ digital-blackboard/src/components/studyHub/AddStudyHubDialog.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-card
+      v-if=""
+      style="padding: 20px; width: 400px;"
+  >
+    <v-card-title>Eintrag erstellen</v-card-title>
+    <form @submit.prevent="submit">
+
+      <v-select
+          v-model="select.value.value"
+          :items="items"
+          :error-messages="select.errorMessage.value"
+          label="Kategorie auswählen"
+          variant="outlined"
+      ></v-select>
+
+      <v-text-field
+          v-model="name.value.value"
+          :counter="10"
+          :error-messages="name.errorMessage.value"
+          label="Titel des Eintrags"
+          variant="outlined"
+      ></v-text-field>
+
+      <v-text-field
+          v-model="email.value.value"
+          :error-messages="email.errorMessage.value"
+          label="E-mail"
+          variant="outlined"
+      ></v-text-field>
+
+      <v-checkbox
+          v-model="subject.value.value"
+          :error-messages="subject.errorMessage.value"
+          label="Themengebiet/e"
+          variant="outlined"
+      ></v-checkbox>
+
+      <v-btn
+          class="me-4"
+          type="submit"
+      >
+        submit
+      </v-btn>
+
+      <v-btn @click="handleReset">
+        clear
+      </v-btn>
+    </form>
+  </v-card>
+</template>
+<script setup>
+
+import { useField, useForm } from 'vee-validate'
+
+const { handleSubmit, handleReset } = useForm({
+  validationSchema: {
+    name (value) {
+      if (value?.length >= 2) return true
+
+      return 'Name needs to be at least 2 characters.'
+    },
+    phone (value) {
+      if (value?.length > 9 && /[0-9-]+/.test(value)) return true
+
+      return 'Phone number needs to be at least 9 digits.'
+    },
+    email (value) {
+      if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+
+      return 'Must be a valid e-mail.'
+    },
+    select (value) {
+      if (value) return true
+
+      return 'Select an item.'
+    },
+    checkbox (value) {
+      if (value === '1') return true
+
+      return 'Must be checked.'
+    },
+  },
+})
+
+const name = useField('name')
+const phone = useField('phone')
+const email = useField('email')
+const select = useField('select')
+const checkbox = useField('checkbox')
+
+const submit = handleSubmit(values => {
+  alert(JSON.stringify(values, null, 2))
+})
+</script>
---
+++ digital-blackboard/src/components/studyHub/StudyHubBuddyCard.vue
@@ -0,0 +1,214 @@
+<template>
+
+  <v-card style="max-width: 700px;">
+    <!-- Images -->
+    <v-carousel
+        :height="item.images.length < 1 ? '0px' : '300px'"
+        :show-arrows="item.images.length > 1 ? 'hover' : false"
+        hide-delimiters
+        progress="primary"
+    >
+      <v-carousel-item
+          v-for="(image, index) in item.images"
+          :key="index"
+          :src="image"
+          cover
+          class="align-end"
+          @click="showDialogImagesFullscreen=true"
+      ></v-carousel-item>
+
+      <v-dialog
+          v-model="showDialogImagesFullscreen"
+          max-width="1200px"
+      >
+        <v-card>
+          <v-row class="d-flex align-center ma-3">
+            <v-card-title>
+              {{ item.title }}
+            </v-card-title>
+
+            <v-btn
+                class="button-round ml-auto mr-1"
+                icon="mdi-close"
+                @click="showDialogImagesFullscreen=false"
+            ></v-btn>
+          </v-row>
+
+          <v-divider></v-divider>
+
+          <v-carousel
+              :show-arrows="item.images.length > 1 ? 'hover' : false"
+              progress="primary"
+          >
+            <v-carousel-item
+                v-for="(image, index) in item.images"
+                :key="index"
+                :src="image"
+            ></v-carousel-item>
+          </v-carousel>
+        </v-card>
+      </v-dialog>
+    </v-carousel>
+
+    <!-- Information shown -->
+    <v-card-title class="align-center">
+      <h4>
+        {{ item.title }}
+      </h4>
+    </v-card-title>
+    <v-card-subtitle
+        margin-top="5px"
+        margin.left="15px"
+    >
+      <p>
+        Einstellungsdatum: {{ item.date_created }}
+      </p>
+    </v-card-subtitle>
+
+    <!-- Show basic information -->
+    <v-card-text class="ma-1">
+      <v-row
+          v-for="(field, index) in basicInfos"
+          :key="index"
+          no-gutters
+          cols="12"
+      >
+        <v-col cols="3">
+          <h4>
+            {{ field.label }}
+          </h4>
+        </v-col>
+        <v-col cols="9">
+          <p>
+            {{ field.value }}
+          </p>
+        </v-col>
+      </v-row>
+
+    </v-card-text>
+
+    <v-rating
+        v-if="item.rating > 0"
+        hover:true
+        readonly:true
+        style="margin-left: 15px; margin-bottom: 10px;"
+        :length="5"
+        :size="35"
+        :model-value="item.rating"
+        color="grey"
+        active-color="red"
+    />
+
+    <v-divider></v-divider>
+
+    <!-- Button: Click for more information -->
+    <span
+        style="display: flex; align-items: center; margin: 10px;"
+    >
+      <v-btn
+          class="button-round"
+          density="compact"
+          :icon="showAll ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+          @click="showAll = !showAll"
+      ></v-btn>
+
+      <p
+          class="ml-3"
+      >
+        Mehr Informationen
+      </p>
+    </span>
+
+    <v-divider></v-divider>
+
+    <!-- Information shown when expanded-->
+    <v-expand-transition>
+      <div v-if="showAll">
+        <v-card-text class="">
+          <v-row
+              v-for="(field, index) in extraInfos"
+              :key="index"
+              no-gutters
+          >
+            <v-col>
+              <h4>
+                {{ field.label }}
+              </h4>
+            </v-col>
+            <v-col>
+              <p>
+                {{ field.value }}
+              </p>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <v-btn
+            class="button-default float-right ma-5 font-weight-medium"
+        >
+          {{ action }}
+        </v-btn>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+      item: Object,
+      action: String,
+  },
+    data() {
+      return {
+        showAll: false,
+        showDialogImagesFullscreen: false,
+
+      basicInfosKeywords: [
+        "price", "subject"
+      ],
+      extraInfosKeywords: [
+        "description", "availability"
+      ],
+      dictionary: {
+        "availability": "Verfügbarkeit",
+        "price" : "Preis pro stunde",
+        "description": "Beschreibung",
+        "subject": "Fächer",
+      }
+    };
+  },
+  computed: {
+    basicInfos() {
+      let basicInfos = [];
+      for (const attribute of this.basicInfosKeywords) {
+        let value = this.item[attribute]
+        basicInfos.push({label: this.dictionary[attribute], value: value});
+      }
+
+      return basicInfos;
+    },
+    extraInfos() {
+      let extraInfos = [];
+      for (const attribute of this.extraInfosKeywords) {
+        let value = this.item[attribute]
+        extraInfos.push({label: this.dictionary[attribute], value: value});
+      }
+
+      return extraInfos;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.button-round {
+  background: white;
+  color: #E0001BFF; /* TODO: Change to color-dhbw-red */
+}
+
+.button-default {
+  background: #E0001BFF;
+  color: white;
+}
+</style>
