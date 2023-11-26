a734206a1a1f1a88cb2d3f36fa0cca4576f9d7be
Niklas Seither <s222792@student.dhbw-mannheim.de>
2023-11-27 00:55:52 +0100

    Unreachable URL replaced

--- digital-blackboard/src/components/studyHub/StudyHubPage.vue
+++ digital-blackboard/src/components/studyHub/StudyHubPage.vue
@@ -77,7 +77,7 @@ export default {
     contents: [
       {
         title: "IBM Udo",
-        images: ["https://cdn.discordapp.com/attachments/685223087133949963/1178108774339330079/PHOTO-2023-05-16-14-04-46.jpg?ex=6574f24c&is=65627d4c&hm=13fa9470ec2efde5e2a0279156b394143c5f6f3c346fc8cb2ba9eb9c7f66df32&"],
+        images: ["https://cdn.discordapp.com/attachments/781425328236658728/1178473529684004904/PHOTO-2023-05-16-14-04-46.jpg?ex=65764600&is=6563d100&hm=71af3f91f443db547d62204cbc2a4e995e5cf7f5a1507356730c343485c98a21&"],
         date_created: "25.11.2023",
         subject: "IBM, Windoofs",
         description: "Ich fahr das besde Audo der Welt und damit mein ich net so nen doofen Tesla",
@@ -114,25 +114,6 @@ export default {
         price: "alles nur Abzocke",
         rating: 5,
         category: "buddy"
-      },
-      {
-        title: "Niggl",
-        date_created: "25.10.2018",
-        description: '"Nenn mich einfach Meister oder Sensei"',
-        subject: "Web Programmierung, Statistik",
-        images: ['https://media.licdn.com/dms/image/D4D03AQE1rqeodXQqtQ/profile-displayphoto-shrink_200_200/0/1691487446798?e=2147483647&v=beta&t=SeFrL8y5GMt4bNt8AHJgM1AXrQOmr5HdVZmMkuwMWbY'],
-        price: "250 €",
-        rating: 1,
-        category: "buddy",
-      },
-      {
-        title: "Die wilden Mäuschen",
-        date_created: "25.10.2018",
-        description: '"Immer voll bei der Sache"',
-        subject: "Web Programmierung, Statistik",
-        activities: "Trinken bis die Sonne wieder untergeht",
-        images: ['https://th.bing.com/th/id/OIP.ZNCLp5fUZ3UDs2Q1KGsuSgHaE7?w=244&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'],
-        category: "group",
       },
     ],
   }),
