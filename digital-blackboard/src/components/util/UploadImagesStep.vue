f5208381de89ae1ca359bf8c33585f989d3c491d
Niklas Seither <s222792@student.dhbw-mannheim.de>
2023-11-28 01:01:56 +0100

    First prototype for uploading images, show a preview and deleting single uploads

--- digital-blackboard/src/components/util/UploadImagesStep.vue
+++ digital-blackboard/src/components/util/UploadImagesStep.vue
@@ -1,37 +1,56 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-row>
-        <v-col cols="12">
-          <v-file-input
-              v-model="selectedFiles"
-              label="Choose images"
-              show-size
-              accept="image/*"
-              multiple
-              @change="previewImages"
-          ></v-file-input>
-        </v-col>
-      </v-row>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-file-input
+            v-model="uploadedImages"
+            label="Choose images"
+            show-size
+            :chips="true"
+            :multiple="true"
+            color="#eb1b2a"
+            accept="image/*"
+            @change="previewImages"
+            @click:clear="clearItems"
+            @select="clearItems"
+        ></v-file-input>
+      </v-col>
+    </v-row>
 
-      <v-row v-if="imagePreviews.length > 0">
-        <v-col cols="12" sm="6" md="4" lg="3" v-for="(preview, index) in imagePreviews" :key="index">
-          <v-card>
-            <v-card-text class="text-h6">{{ selectedFiles[index].name }}</v-card-text>
-            <v-img :src="preview" contain max-height="200px"></v-img>
-
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app>
+    <v-row v-if="uploadedImages.length > 0">
+      <v-col
+          v-for="(preview, index) in imagePreviews"
+          :key="index"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+      >
+        <v-card v-if="imagePreviews.length > 0">
+          <v-card-text class="text-h7">{{ uploadedImages[index].name }}</v-card-text>
+          <v-divider></v-divider>
+          <v-img :src="preview" contain max-height="200px"></v-img>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn
+                color="#eb1b2a"
+                class="ml-auto"
+                @click="deleteImage(index)"
+            >
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      selectedFiles: [],
+      uploadedImages: [],
       imagePreviews: [],
     };
   },
@@ -39,7 +58,7 @@ export default {
     previewImages() {
       this.imagePreviews = [];
 
-      this.selectedFiles.forEach((file) => {
+      this.uploadedImages.forEach((file) => {
         const reader = new FileReader();
         reader.onload = (e) => {
           this.imagePreviews.push(e.target.result);
@@ -47,10 +66,18 @@ export default {
         reader.readAsDataURL(file);
       });
     },
+    clearItems() {
+      this.uploadedImages = [];
+      this.imagePreviews = [];
+    },
+    deleteImage(index) {
+      this.uploadedImages.splice(index, 1);
+      this.imagePreviews.splice(index, 1);
+    },
   },
 };
 </script>
 
 <style scoped>
-/* Add your custom styles here */
+
 </style>
