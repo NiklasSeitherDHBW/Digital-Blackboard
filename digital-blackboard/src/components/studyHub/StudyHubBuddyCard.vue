336d4c1da7457ef2e938f1d8137aa7e1f49558e9
Niklas Seither <s222792@student.dhbw-mannheim.de>
2023-12-02 12:48:11 +0100

    Implemented showing dialog for contact information when button is clicked

--- digital-blackboard/src/components/studyHub/StudyHubBuddyCard.vue
+++ digital-blackboard/src/components/studyHub/StudyHubBuddyCard.vue
@@ -1,25 +1,41 @@
 <template>
   <CustomCard
+      class="text-left"
       :item="item"
       :basicInfos="basicInfos"
       :extraInfos="extraInfos"
       action="Kontaktieren"
+      @action-clicked="showDialogContactInfo=true"
   ></CustomCard>
+
+  <v-dialog
+      v-model="showDialogContactInfo"
+      transition="dialog-bottom-transition"
+  >
+    <ContactCard
+        :name="item.title"
+        :phone="item.phone"
+        :email="item.email"
+        @close-dialog="showDialogContactInfo=false"
+    ></ContactCard>
+  </v-dialog>
 </template>
 
 <script>
 import CustomCard from "@/components/util/CustomCard.vue";
+import ContactCard from "@/components/util/ContactCard.vue";
 
 export default {
-  components: {CustomCard},
+  components: {ContactCard, CustomCard},
   props: {
-      item: Object,
-      action: String,
-  },
-    data() {
-      return {
-        showAll: false,
-        showDialogImagesFullscreen: false,
+    item: Object,
+    action: String,
+    },
+  data() {
+    return {
+      showAll: false,
+      showDialogImagesFullscreen: false,
+      showDialogContactInfo: false,
 
       basicInfosKeywords: [
         "price", "subject"
@@ -29,7 +45,7 @@ export default {
       ],
       dictionary: {
         "availability": "Verfügbarkeit",
-        "price" : "Preis pro stunde in €",
+        "price": "Preis pro stunde in €",
         "description": "Beschreibung",
         "subject": "Fächer",
       }
