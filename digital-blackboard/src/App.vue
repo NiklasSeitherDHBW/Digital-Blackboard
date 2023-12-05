10563af4ff775e5582b50dbc624a0870e67ec945
yannick <ykoenigstein68@gmail.com>
2023-12-06 00:10:14 +0100

    NavBar jetzt in App.vue

--- digital-blackboard/src/App.vue
+++ digital-blackboard/src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <v-app>
     <v-main>
-      <router-view/>
+      <CustomNavBar />
+      <router-view />
+      <v-snackbar v-model="snackbarVisible">{{ snackbarText }}</v-snackbar>
     </v-main>
   </v-app>
 </template>
 
-<script>
+<script setup>
+import CustomNavBar from "@/components/util/CustomNavBar.vue";
+</script>
 
+<script>
 export default {
   name: 'App',
 
   data: () => ({
-    //
+    snackbarText: '',
+    snackbarVisible: false,
+    timeout: 3000,
   }),
+
+  methods: {
+    showSnackbar(text, timeout = 3000) {
+      this.snackbarText = text;
+      this.timeout = timeout;
+      this.snackbarVisible = true;
+    },
+  }
 }
 </script>
 
