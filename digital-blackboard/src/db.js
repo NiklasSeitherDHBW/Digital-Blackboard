4a2b6b66eb9b8087049ff343bdd527e4e52bae6c
s222792 <150899939+s222792@users.noreply.github.com>
2023-12-07 00:28:39 +0100

    Added functions for creating events and studyhub advertisements, added delete function

--- digital-blackboard/src/db.js
+++ digital-blackboard/src/db.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import {initializeApp} from 'firebase/app'
+import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBPCSF20BXJ_qysy7MRnYpfwa08Yplo6sw",
@@ -13,9 +13,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
-export { db };
+export {db};
 
-import {collection, getDocs, addDoc, Timestamp} from 'firebase/firestore'
+import {collection, getDocs, addDoc, deleteDoc, doc, Timestamp} from 'firebase/firestore'
 
 // All database operations regarding dual living page
 
@@ -74,7 +74,8 @@ export async function createAdDualLiving(formData, images, contactData) {
         date_created: Timestamp.fromDate(new Date()),
 
         available_from: Timestamp.fromDate(new Date(fromParts[0], fromParts[1] - 1, fromParts[2])), // parts[1] - 1 because JavaScript counts months from 0 (January - 1, Februaray - 2, etc.)
-        available_until: Timestamp.fromDate(new Date(untilParts[0], fromParts[1] - 1, fromParts[2])),
+        available_until: Timestamp.fromDate(new Date(untilParts[0], untilParts[1] - 1, untilParts[2])),
+
         area: formData.area,
         rooms: formData.rooms,
         price: formData.price,
@@ -116,8 +117,7 @@ export async function fetchAdsEventsInfos() {
             tmp["images"] = tmp["images"].length > 0 ? tmp["images"] : ["https://media.discordapp.net/attachments/1097461286599659600/1181540706087612467/Kevkoo118_Students_celebrating_a_partytwhite_background_red_gre_56eda253-920d-488d-8d9b-87f7da47f1c9.png?ex=65816e88&is=656ef988&hm=bce22ece5f3172f67047fb1c9c1e239686cf61e01efcbbfa109232ace8e2af72&=&width=1714&height=960"]
         } else if (tmp["category"] === "Infos") {
             tmp["images"] = tmp["images"].length > 0 ? tmp["images"] : ["https://media.discordapp.net/attachments/1097461286599659600/1181650618293686332/Kevkoo118_Ais_receiving_information_student_white_background_re_2f690c7c-7719-4805-9456-986f05102733.png?ex=6581d4e5&is=656f5fe5&hm=19b8d726f5ebab80ee7cff1b9d744c50dd71d8b2ae44d9e42fbf47a430c632ea&=&width=1642&height=920"]
-        }
-        else if (tmp["category"] === "Seminare") {
+        } else if (tmp["category"] === "Seminare") {
             tmp["images"] = tmp["images"].length > 0 ? tmp["images"] : ["https://media.discordapp.net/attachments/1097461286599659600/1181649925302395011/Kevkoo118_students_listen_to_a_seminar_white_background_red_gre_ba29e608-8c8f-4c0b-afec-2f8ac6884f53.png?ex=6581d440&is=656f5f40&hm=964c1d3d799dddc4c029033cfe38e6d20b83ca84149190848a626bf282162d63&=&width=1642&height=920"]
         }
 
@@ -143,6 +143,85 @@ export async function fetchAdsEventsInfos() {
     return transformedData;
 }
 
+export async function createAdEvents(images, eventData) {
+    console.log(images, eventData)
+
+    // Split date by every component to create a date object for firebase
+    let dateParts = eventData.date.split("-")
+
+    let new_item = {
+        images: images,
+
+        title: eventData.title,
+        date_created: Timestamp.fromDate(new Date()),
+
+        date: Timestamp.fromDate(new Date(dateParts[0], dateParts[1] - 1, dateParts[2])), // parts[1] - 1 because JavaScript counts months from 0 (January - 1, Februaray - 2, etc.)
+        price: eventData.price,
+        community: eventData.community,
+
+        description: eventData.description,
+        location: eventData.location,
+        members: 0,
+        max_participants_limit: eventData.maxParticipantsLimit,
+
+        category: eventData.category,
+
+        userId: eventData.userId,
+    }
+
+    await addDoc(collection(db, "events-parties"), new_item);
+}
+
+export async function createAdInfo(images, infoData) {
+    let dateParts = infoData.date.split("-")
+
+    let new_item = {
+        images: images,
+
+        title: infoData.title,
+        date_created: Timestamp.fromDate(new Date()),
+
+        date: Timestamp.fromDate(new Date(dateParts[0], dateParts[1] - 1, dateParts[2])), // parts[1] - 1 because JavaScript counts months from 0 (January - 1, Februaray - 2, etc.)
+        community: infoData.community,
+
+        description: infoData.description,
+        location: infoData.location,
+
+        category: infoData.category,
+
+        userId: 1,
+    }
+
+    await addDoc(collection(db, "events-parties"), new_item);
+}
+
+export async function createAdSeminar(images, seminarData) {
+    let dateParts = seminarData.date.split("-")
+
+    let new_item = {
+        images: images,
+
+        title: seminarData.title,
+        date_created: Timestamp.fromDate(new Date()),
+
+        date: Timestamp.fromDate(new Date(dateParts[0], dateParts[1] - 1, dateParts[2])), // parts[1] - 1 because JavaScript counts months from 0 (January - 1, Februaray - 2, etc.)
+        price: seminarData.price,
+        community: seminarData.community,
+
+        description: seminarData.description,
+        location: seminarData.location,
+
+        members: 0,
+        max_participants_limit: seminarData.maxParticipantsLimit,
+
+        category: seminarData.category,
+
+        userId: 1
+    }
+
+    await addDoc(collection(db, "events-parties"), new_item);
+}
+
 
 // All database operations regarding study hub page
 
@@ -159,7 +238,7 @@ export async function fetchAdsStudyHub() {
 
         if (tmp["category"] === "buddy") {
             tmp["images"] = tmp["images"].length > 0 ? tmp["images"] : ["https://media.discordapp.net/attachments/1097461286599659600/1181540596486258768/Kevkoo118_default_picture_for_dual_living_white_background_red__4ddb008d-6598-4f15-b97e-b150a008756f.png?ex=65816e6e&is=656ef96e&hm=47454a08ee50768dcf22730502a54e8afd373b9e482eee16f0adee307ec17f12&=&width=1714&height=960"]
-        } else if(tmp["category"] === "group") {
+        } else if (tmp["category"] === "group") {
             tmp["images"] = tmp["images"].length > 0 ? tmp["images"] : ["https://media.discordapp.net/attachments/1097461286599659600/1181651199024443483/Kevkoo118_A_group_of_students_are_learning_white_background_red_13ac0a81-fb88-4c8d-b2cc-9054bd41926c.png?ex=6581d56f&is=656f606f&hm=3182fc97187e3cc68a1905aacef8251f336f12a33d3826eb85442a5b0e3dc8c8&=&width=1642&height=920"]
         }
         // Display dates in format TT.MM.YYYY
@@ -175,4 +254,58 @@ export async function fetchAdsStudyHub() {
     });
 
     return transformedData
+}
+
+export async function createAdStudyBuddy(buddyData, images, contactData) {
+    let new_item = {
+        images: images,
+
+        title: buddyData.title,
+        date_created: Timestamp.fromDate(new Date()),
+
+        price: buddyData.price,
+        subject: buddyData.subject,
+
+        description: buddyData.description,
+        availability: buddyData.availability,
+
+        name: contactData.name,
+        phone: contactData.phone,
+        email: contactData.email,
+
+        category: buddyData.category,
+
+        userId: buddyData.userId,
+    }
+
+    await addDoc(collection(db, "study-hub"), new_item);
+}
+
+export async function createAdStudyGroup(hubData, images) {
+    let new_item = {
+        images: images,
+
+        title: hubData.title,
+        date_created: Timestamp.fromDate(new Date()),
+
+        subject: hubData.subject,
+        members: hubData.members,
+
+        description: hubData.description,
+        activities: hubData.activities,
+
+        joined: hubData.joined,
+        category: "group",
+
+        userId: 1,
+    }
+
+    await addDoc(collection(db, "study-hub"), new_item);
+}
+
+
+// Database operations to delete advertisements
+
+export async function deleteAd(collection, id) {
+    await deleteDoc(doc(db, collection, id));
 }
